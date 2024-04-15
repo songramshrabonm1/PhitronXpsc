7fe75ef93bd3936ef226d36d3ad59aec8e13ab15
songramshrabonm1 <108411769+songramshrabonm1@users.noreply.github.com>
2024-04-15 23:50:56 +0600

    Add files via upload
    week - 4 codes added

---
+++ XPSC/Week-4/S - Petr and a Combination Lock.cpp
@@ -0,0 +1,212 @@
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+// #include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+// #include <cuchar>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#if __cplusplus >= 201402L
+#include <shared_mutex>
+#endif
+
+#if __cplusplus >= 201703L
+#include <any>
+#include <charconv>
+// #include <execution>
+#include <filesystem>
+#include <optional>
+// #include <memory_resource>
+#include <string_view>
+#include <variant>
+#endif
+
+#if __cplusplus >= 202002L
+#include <barrier>
+#include <bit>
+#include <compare>
+#include <concepts>
+#if __cpp_impl_coroutine
+# include <coroutine>
+#endif
+#include <latch>
+#include <numbers>
+// #include <ranges>
+#include <span>
+// #include <stop_token>
+#include <semaphore>
+// #include <source_location>
+// #include <syncstream>
+#include <version>
+#endif
+
+#if __cplusplus > 202002L
+#include <spanstream>
+#endif
+//#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5;
+const int Prime = 1e9+7;
+const int Base = 251;
+
+
+//...................Songram Shrabon..........................//
+#define endl                cout<<"\n";
+#define ll                  long long int
+#define gcd(x, y)           (__gcd(x, y))
+#define lcm(x, y)           ((x/gcd(x, y))*y)
+#define rand(x, y)          rand()%(y-x+1)+x
+#define pow2(x)             (1<<x)
+#define pi                  3.141592653589793
+#define decimal(x, y)       fixed<<setprecision(x)<<y
+#define TxtI                freopen("input.txt","r",stdin);
+#define TxtO                freopen("output.txt","w",stdout);
+#define str                 string;
+#define pair                  pair<ll,ll>
+#define vec                 vector<ll>
+#define py                  cout<<"YES\n"
+#define pn                  cout<<"NO\n"
+#define pb(x)               push_back(x)
+//....................Radha Raman.............................//
+
+
+int main()
+{
+    //srand(time(0));
+    //TxtI TxtO
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i = 0 ;i<n ;i++){
+        cin>>a[i];
+    }
+        
+        vector<vector<int> > allBit;
+        // int Power = 1<<n;
+        for(int i = 0 ;i< (1<<n); i++){
+            vector<int> v;
+                int I = i ; 
+            for(int pos = 0 ; pos<n ;pos++){
+                if((I>>pos) & 1 == 1){
+                    v.push_back(1);
+                }else{
+                    v.push_back(0);
+                }
+            }
+
+            allBit.push_back(v);
+
+        }
+        bool flag = false;
+        for(int i = 0 ;i< (1<<n); i++){
+            int Sum = 0 ; 
+            for(int j = 0 ; j<n ; j++){
+                int aa = allBit[i][j];
+                if(aa == 0){
+                    Sum -=a[j];
+                }else{
+                    Sum+=a[j];
+                }
+            }
+            if(Sum == 0 || Sum == 360){
+                flag= true;
+                py;
+                break;
+            }else if(Sum % 360 == 0){
+                flag = true;
+                py;
+                break;
+            }
+        }
+        if(flag == false){
+            pn;
+
+        }
+
+    
+}
